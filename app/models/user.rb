9823d0fa24a5d747217872279f3600536685a5c6
Peter Pao-Huang <peter.paohuang@gmail.com>
2018-05-21 17:31:09 -0500

    User shit

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  	devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+   	validates :auth_token, uniqueness: true
+   	def generate_authentication_token!
+	    begin
+	      self.auth_token = Devise.friendly_token
+	    end while self.class.exists?(auth_token: auth_token)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+
+  namespace :api , defaults: { format: :json } do
+    # We are going to list our resources here
+    resources :users, :only => [:show, :create, :update, :destroy, :index]
+  	resources :sessions, :only => [:create, :destroy]
+  end
 end
